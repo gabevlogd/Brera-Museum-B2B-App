7e9dfccfddcbd3fb03eac301d12721b3ba9c6620
Alien089 <gabri.moratelli@gmail.com>
2023-11-29 17:56:31 +0100

    continue to work on puzzle editor

--- Assets/Scripts/Puzzle/PuzzleEditor/PuzzleEditor.cs
+++ Assets/Scripts/Puzzle/PuzzleEditor/PuzzleEditor.cs
@@ -11,29 +11,63 @@ using System.IO;
 public class PuzzleEditor : EditorWindow
 {
     private Grid<Toggle> m_GridCoordinates;
-    private int m_PuzzleWidth  = 0;
-    private int m_PuzzleHeigth = 0;
+    private List<Vector2> m_StartingPoint = new List<Vector2>();
+    private List<Vector2> m_EndingPoint = new List<Vector2>();
+    private int m_PuzzleWidth = 10;
+    private int m_PuzzleHeigth = 10;
     private string m_PictureID;
     private string m_PuzzleID;
 
     private string LEVELS_FOLDER_PATH = "Assets/Scripts/ScriptableObjects/Puzzles";
     private Vector2 m_ScrollPos;
 
+    public int PuzzleWidth
+    {
+        get => m_PuzzleWidth;
+
+        set
+        {
+            if (m_PuzzleWidth == value) return;
+
+            m_PuzzleWidth = value;
+            m_GridCoordinates = new Grid<Toggle>(PuzzleWidth, PuzzleHeigth, 1, new Vector3(-3f, 0f, -3f), (int x, int y) => new Toggle(x, y));
+        }
+    }
+
+    public int PuzzleHeigth
+    {
+        get => m_PuzzleHeigth;
+        set
+        {
+            if (m_PuzzleHeigth == value) return;
+
+            m_PuzzleHeigth = value;
+            m_GridCoordinates = new Grid<Toggle>(PuzzleWidth, PuzzleHeigth, 1, new Vector3(-3f, 0f, -3f), (int x, int y) => new Toggle(x, y));
+        }
+    }
+
     [MenuItem("Tools/Puzzle Editor")]
-    public static void ShowWindow() => GetWindow<PuzzleEditor>("Level Editor");
+    public static void ShowWindow() => GetWindow<PuzzleEditor>("Puzzle Editor");
 
     private void OnEnable()
     {
-        m_GridCoordinates = new Grid<Toggle>(m_PuzzleWidth, m_PuzzleHeigth, 1, new Vector3(-3f, 0f, -3f), (int x, int y) => new Toggle(x, y));
+        m_GridCoordinates = new Grid<Toggle>(PuzzleWidth, PuzzleHeigth, 1, new Vector3(-3f, 0f, -3f), (int x, int y) => new Toggle(x, y));
     }
+
     void OnGUI()
     {
         m_ScrollPos = EditorGUILayout.BeginScrollView(m_ScrollPos);
         DrawPuzzleInformationsInputField();
         DrawPuzzleWidthInputField();
         DrawPuzzleHeigthInputField();
+        GUILayout.Space(10);
+        DrawListOfStartingPoint();
+        GUILayout.Space(10);
+        DrawListOfEndingPoint();
+        GUILayout.Space(10);
         DrawSaveLevelButton();
         DrawTogglesGrid();
+        GUILayout.Space(50);
         EditorGUILayout.EndScrollView();
     }
 
@@ -56,7 +90,7 @@ public class PuzzleEditor : EditorWindow
         GUILayout.BeginHorizontal();
         GUILayout.FlexibleSpace();
         GUILayout.Label("Puzzle Width: ");
-        m_PuzzleWidth = EditorGUILayout.IntField(m_PuzzleWidth);
+        PuzzleWidth = EditorGUILayout.IntField(PuzzleWidth);
         GUILayout.FlexibleSpace();
         GUILayout.EndHorizontal();
     }
@@ -67,9 +101,55 @@ public class PuzzleEditor : EditorWindow
         GUILayout.BeginHorizontal();
         GUILayout.FlexibleSpace();
         GUILayout.Label("Puzzle Heigth: ");
-        m_PuzzleHeigth = EditorGUILayout.IntField(m_PuzzleHeigth);
+        PuzzleHeigth = EditorGUILayout.IntField(PuzzleHeigth);
         GUILayout.FlexibleSpace();
         GUILayout.EndHorizontal();
+    }
+
+    private void DrawListOfStartingPoint()
+    {
+        EditorGUILayout.BeginVertical(GUI.skin.box);
+        GUILayout.Label("List of Starting Points", EditorStyles.boldLabel);
+
+        for (int i = 0; i < m_StartingPoint.Count; i++)
+        {
+            EditorGUILayout.BeginHorizontal(GUI.skin.box);
+            m_StartingPoint[i] = EditorGUILayout.Vector2Field("Element " + i, m_StartingPoint[i]);
+            if (GUILayout.Button(" - ", GUILayout.MaxHeight(40), GUILayout.MaxWidth(32)))
+                m_StartingPoint.RemoveAt(i);
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        if (GUILayout.Button(" + ", GUILayout.MaxWidth(32)))
+            m_StartingPoint.Add(new Vector2());
+
+        EditorGUILayout.EndVertical();
+
+        GUILayout.Space(10);
+    }
+
+    private void DrawListOfEndingPoint()
+    {
+        EditorGUILayout.BeginVertical(GUI.skin.box);
+        GUILayout.Label("List of Ending Points", EditorStyles.boldLabel);
+
+        for (int i = 0; i < m_EndingPoint.Count; i++)
+        {
+            EditorGUILayout.BeginHorizontal(GUI.skin.box);
+            m_EndingPoint[i] = EditorGUILayout.Vector2Field("Element " + i, m_EndingPoint[i]);
+            if (GUILayout.Button(" - ", GUILayout.MaxHeight(40), GUILayout.MaxWidth(32)))
+                m_StartingPoint.RemoveAt(i);
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        if (GUILayout.Button(" + ", GUILayout.MaxWidth(32)))
+            m_EndingPoint.Add(new Vector2());
+
+        EditorGUILayout.EndVertical();
+
+        GUILayout.Space(10);
     }
 
     private void DrawSaveLevelButton()
@@ -88,7 +168,7 @@ public class PuzzleEditor : EditorWindow
         if (!Directory.Exists($"{LEVELS_FOLDER_PATH}/{m_PictureID}"))
             AssetDatabase.CreateFolder($"{LEVELS_FOLDER_PATH}", $"{m_PictureID}");
 
-        AssetDatabase.CreateAsset(newLevel, $"{LEVELS_FOLDER_PATH}/{m_PictureID}/Puzzle {m_PuzzleID}.asset");
+        AssetDatabase.CreateAsset(newLevel, $"{LEVELS_FOLDER_PATH}/Picture {m_PictureID}/Puzzle {m_PuzzleID}.asset");
         AssetDatabase.SaveAssets();
         Debug.Log("New puzzle saved");
     }
@@ -106,11 +186,11 @@ public class PuzzleEditor : EditorWindow
         GUILayout.EndHorizontal();
 
         GUILayout.BeginVertical();
-        for (int y = 0; y < m_PuzzleHeigth; y++)
+        for (int y = 0; y < PuzzleHeigth; y++)
         {
             GUILayout.BeginHorizontal();
             GUILayout.FlexibleSpace();
-            for (int x = 0; x < m_PuzzleWidth; x++)
+            for (int x = 0; x < PuzzleWidth; x++)
                 m_GridCoordinates.GetGridObject(x, y).Value = GUILayout.Toggle(m_GridCoordinates.GetGridObject(x, y).Value, "", GUILayout.Width(20f), GUILayout.Height(20f));
             GUILayout.FlexibleSpace();
             GUILayout.EndHorizontal();
@@ -122,14 +202,30 @@ public class PuzzleEditor : EditorWindow
     {
         PuzzleData newPuzzle = CreateInstance<PuzzleData>();
 
-        newPuzzle.Grid = new Grid<Node>(m_PuzzleWidth, m_PuzzleHeigth, 1, new Vector3(-3f, 0f, -3f), (int x, int y) => new Node(x, y));
-        newPuzzle.GridWidth = m_PuzzleWidth;
-        newPuzzle.GridHeight = m_PuzzleHeigth;
+        newPuzzle.Grid = new Grid<Node>(PuzzleWidth, PuzzleHeigth, 1, new Vector3(-3f, 0f, -3f), (int x, int y) => new Node(x, y));
+        newPuzzle.GridWidth = PuzzleWidth;
+        newPuzzle.GridHeight = PuzzleHeigth;
 
-        for (int y = 0; y < m_PuzzleHeigth; y++)
+        for (int y = 0; y < PuzzleHeigth; y++)
         {
-            for (int x = 0; x < m_PuzzleWidth; x++)
+            for (int x = 0; x < PuzzleWidth; x++)
+            {
+                bool isFounded = false;
+                if (isFounded == false)
+                {
+                    foreach (Vector2 pos in m_StartingPoint)
+                    {
+                        if (new Vector2(x, y) == pos)
+                        {
+                            newPuzzle.Grid.GetGridObject(x, y).SetNode(NodeType.Start, !m_GridCoordinates.GetGridObject(x, y).Value);
+                            isFounded = true;
+                            break;
+                        }
+                    }
+                }
+
                 newPuzzle.Grid.GetGridObject(x, y).SetNode(NodeType.Start, !m_GridCoordinates.GetGridObject(x, y).Value);
+            }
         }
 
         DirectoryInfo info = new DirectoryInfo($"{LEVELS_FOLDER_PATH}/x/y");
