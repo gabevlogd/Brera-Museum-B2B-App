52c8cfa15f4556d4f2f7dba0fb01055919ee8c18
Alien089 <gabri.moratelli@gmail.com>
2023-12-19 13:53:35 +0100

    created prefab vuforia and finished it

--- Assets/Scripts/AR/ARManager.cs
+++ Assets/Scripts/AR/ARManager.cs
@@ -5,8 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class ARManager : MonoBehaviour
 {
+    [SerializeField] private Camera m_ARCamera;
     public void ImageVisibile(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
+    }
+
+    //private void OnEnable()
+    //{
+    //    m_ARCamera.transform.GetChild(0).gameObject.layer = 7;
+    //}
+
+    private void Start()
+    {
+        m_ARCamera.transform.GetChild(0).gameObject.layer = 7;
     }
 }
